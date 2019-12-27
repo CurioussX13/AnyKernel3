18fed19eaed40ffbacf5620cc60c97a4bdb125b3
CurioussX13 <58982087+CurioussX13@users.noreply.github.com>
2019-12-27 09:09:42 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@
 # begin properties
 properties() { '
 kernel.string=Clarity Kernel for Xiaomi Redmi Note 4(x) Snapdragon
-do.devicecheck=1
+do.devicecheck=0
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=1
